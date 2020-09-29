9613784fd6a43ab57e3a16564a33cb837b79da7a
Murilo Alves <muriloalvesdev.ti@gmail.com>
2020-09-29 19:12:48 -0300

    project init

---
+++ src/main/java/br/com/developers/GithubDevelopersApplication.java
@@ -0,0 +1,13 @@
+package br.com.developers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDevelopersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDevelopersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/developers/GithubDevelopersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.developers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDevelopersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
