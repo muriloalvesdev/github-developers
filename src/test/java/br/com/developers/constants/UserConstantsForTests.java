405b16978de34bdd85c1e54da95d49d34cb86f28
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-30 19:34:02 -0300

    add constants for tests

--- src/test/java/br/com/developers/constants/UserConstantsForTests.java
+++ src/test/java/br/com/developers/constants/UserConstantsForTests.java
@@ -5,5 +5,7 @@ public interface UserConstantsForTests {
   static final String EMAIL = "murilohenrique.ti@outlook.com.br";
   static final String FIRST_NAME = "Murilo";
   static final String LAST_NAME = "Batista";
-
+  static final String PASSWORD = "123456";
+  static final String TOKEN =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 }
