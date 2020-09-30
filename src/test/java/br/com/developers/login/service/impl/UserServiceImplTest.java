152dcef4a8fb7043f0fd0b9143ca8fa9b456dc11
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-30 19:35:54 -0300

    test for userServiceImpl

---
+++ src/test/java/br/com/developers/login/service/impl/UserServiceImplTest.java
@@ -0,0 +1,112 @@
+package br.com.developers.login.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import java.util.HashSet;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.mockito.BDDMockito;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import br.com.developers.config.jwt.JwtProvider;
+import br.com.developers.constants.UserConstantsForTests;
+import br.com.developers.login.domain.model.Role;
+import br.com.developers.login.domain.model.Role.RoleName;
+import br.com.developers.login.domain.model.User;
+import br.com.developers.login.domain.repository.RoleRepository;
+import br.com.developers.login.domain.repository.UserRepository;
+import br.com.developers.login.dto.RegisterDTO;
+import br.com.developers.login.exception.ExistingEmailException;
+import br.com.developers.login.exception.IllegalRoleException;
+import br.com.developers.provider.RegisterDTOProviderTests;
+
+public class UserServiceImplTest implements UserConstantsForTests {
+
+  private UserRepository userRepository;
+  private RoleRepository roleRepository;
+  private PasswordEncoder encoder;
+  private JwtProvider jwtProvider;
+  private AuthenticationManager authenticationManager;
+  private UserServiceImpl service;
+
+  private Role role;
+  private User user;
+
+  @BeforeEach
+  void setUp() {
+    HashSet<Role> roles = new HashSet<>();
+    this.role = Role.newBuilder().name(RoleName.ROLE_ADMIN).id(1L).build();
+    roles.add(this.role);
+    this.user = User.newBuilder().email(EMAIL).firstName(FIRST_NAME).lastName(LAST_NAME)
+        .roles(roles).build();
+
+    this.userRepository = spy(UserRepository.class);
+    this.roleRepository = spy(RoleRepository.class);
+    this.encoder = spy(PasswordEncoder.class);
+    this.jwtProvider = spy(JwtProvider.class);
+    this.authenticationManager = spy(AuthenticationManager.class);
+    this.service = new UserServiceImpl(this.userRepository, this.roleRepository, this.encoder,
+        this.jwtProvider, this.authenticationManager);
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(RegisterDTOProviderTests.class)
+  void shouldRegiterUser(RegisterDTO registerData) {
+    BDDMockito.given(this.roleRepository.findByName(RoleName.ROLE_ADMIN))
+        .willReturn(Optional.of(this.role));
+    BDDMockito.given(this.encoder.encode(registerData.getPassword())).willReturn(anyString());
+    BDDMockito.given(this.userRepository.existsByEmail(EMAIL)).willReturn(Boolean.FALSE);
+    BDDMockito.given(this.userRepository.saveAndFlush(this.user)).willReturn(any(User.class));
+
+    this.service.registerUser(registerData);
+
+    verify(this.userRepository, times(1)).existsByEmail(anyString());
+    verify(this.encoder, times(1)).encode(anyString());
+    verify(this.roleRepository, times(1)).findByName(any());
+    verify(this.userRepository, times(1)).saveAndFlush(any(User.class));
+
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(RegisterDTOProviderTests.class)
+  void shouldReturnExistingEmailException(RegisterDTO registerData) {
+    BDDMockito.given(this.userRepository.existsByEmail(EMAIL)).willReturn(Boolean.TRUE);
+
+    Exception exception = assertThrows(Exception.class, () -> {
+      this.service.registerUser(registerData);
+    });
+
+    assertTrue(exception instanceof ExistingEmailException);
+    assertEquals("Failed -> Email is already in use!", exception.getMessage());
+
+    verify(this.userRepository, times(1)).existsByEmail(anyString());
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(RegisterDTOProviderTests.class)
+  void shouldReturnIllegalRoleException(RegisterDTO registerData) {
+
+    HashSet<String> roles = new HashSet<>();
+    roles.add("teste");
+    registerData.setRole(roles);
+
+    BDDMockito.given(this.roleRepository.findByName(RoleName.ROLE_ADMIN))
+        .willReturn(Optional.empty());
+
+    Exception exception = assertThrows(Exception.class, () -> {
+      this.service.registerUser(registerData);
+    });
+
+    assertTrue(exception instanceof IllegalRoleException);
+    assertEquals("Fail! -> Cause: Role invalid.", exception.getMessage());
+  }
+
+}
