6ff2846b292f14e1c33976c2f47611471ddb3659
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-30 19:58:18 -0300

    create enum roleName with logic itself

---
+++ src/main/java/br/com/developers/login/domain/model/RoleName.java
@@ -0,0 +1,25 @@
+package br.com.developers.login.domain.model;
+
+import java.util.Arrays;
+import br.com.developers.login.exception.IllegalRoleException;
+
+public enum RoleName {
+  ROLE_ADMIN("admin");
+
+  private static final String ROLE_INVALID = "Fail! -> Cause: %s is Role invalid.";
+  private String name;
+
+  private RoleName(String name) {
+    this.name = name;
+  }
+
+  private String getName() {
+    return this.name;
+  }
+
+  public static RoleName find(String roleName) {
+    return Arrays.asList(RoleName.values()).stream()
+        .filter(r -> r.getName().equals(roleName.toLowerCase())).findAny()
+        .orElseThrow(() -> new IllegalRoleException(String.format(ROLE_INVALID, roleName)));
+  }
+}
