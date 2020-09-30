f9cb9619ef720c03dee5420783ec81604b2ae134
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-29 21:43:43 -0300

    constants user for tests

---
+++ src/test/java/br/com/developers/constants/UserConstantsForTests.java
@@ -0,0 +1,9 @@
+package br.com.developers.constants;
+
+public interface UserConstantsForTests {
+
+  static final String EMAIL = "murilohenrique.ti@outlook.com.br";
+  static final String FIRST_NAME = "Murilo";
+  static final String LAST_NAME = "Batista";
+
+}
