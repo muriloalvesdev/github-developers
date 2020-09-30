4e0ea3f296b15cbb65e00f55fbd5fba31e9919aa
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-30 20:12:45 -0300

    fields required

--- src/main/java/br/com/developers/login/dto/LoginDTO.java
+++ src/main/java/br/com/developers/login/dto/LoginDTO.java
@@ -1,9 +1,14 @@
 package br.com.developers.login.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class LoginDTO {
+  @Email(message = "email is invalid")
   private String email;
+
+  @NotNull(message = "password is required")
   private String password;
 }
