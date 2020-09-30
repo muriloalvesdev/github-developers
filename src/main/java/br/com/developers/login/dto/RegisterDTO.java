f766b43634ea978a39def6b96d701f42d11535b8
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-30 19:33:22 -0300

    validations in registerDTO

--- src/main/java/br/com/developers/login/dto/RegisterDTO.java
+++ src/main/java/br/com/developers/login/dto/RegisterDTO.java
@@ -2,14 +2,20 @@ package br.com.developers.login.dto;
 
 import java.util.Set;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class RegisterDTO {
+  @NotNull(message = "name is required")
   private String name;
+
+  @NotNull(message = "last_name is required")
+  @JsonProperty("last_name")
   private String lastName;
 
-  @Email
+  @Email(message = "email is invalid")
   private String email;
   private Set<String> role;
   private String password;
