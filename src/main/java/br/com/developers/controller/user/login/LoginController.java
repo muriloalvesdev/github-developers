651d0435a74497f1f05d2895f381b526599d2a3c
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-30 19:35:40 -0300

    controller for login

---
+++ src/main/java/br/com/developers/controller/user/login/LoginController.java
@@ -0,0 +1,29 @@
+package br.com.developers.controller.user.login;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.developers.login.dto.LoginDTO;
+import br.com.developers.login.http.request.AccessToken;
+import br.com.developers.login.service.UserService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/auth")
+public class LoginController {
+
+  private UserService userService;
+
+
+  @PostMapping
+  public ResponseEntity<AccessToken> authenticateUser(@Validated @RequestBody LoginDTO loginData) {
+    return ResponseEntity.ok().body(userService.authenticateUser(loginData));
+  }
+}
