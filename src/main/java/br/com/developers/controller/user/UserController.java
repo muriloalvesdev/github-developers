170d05e0e7acab26d8e66a1980b760ed917e8caf
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-09-30 21:08:34 -0300

    rest of the APIs to complete a CRUD (find, update, delete)

--- src/main/java/br/com/developers/controller/user/UserController.java
+++ src/main/java/br/com/developers/controller/user/UserController.java
@@ -1,14 +1,20 @@
 package br.com.developers.controller.user;
 
-import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.com.developers.domain.model.User;
+import br.com.developers.login.dto.LoginDTO;
 import br.com.developers.login.dto.RegisterDTO;
 import br.com.developers.login.service.UserService;
 import lombok.AccessLevel;
@@ -23,12 +29,31 @@ public class UserController {
   private UserService userService;
 
   @PostMapping("/")
-  public ResponseEntity<User> registerUser(@Valid @RequestBody RegisterDTO registerData) {
-    User user = userService.registerUser(registerData);
-
+  public ResponseEntity<Object> registerUser(@Validated @RequestBody RegisterDTO registerData) {
+    User user = this.userService.registerUser(registerData);
     return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
         .buildAndExpand(user.getId()).toUri()).build();
+  }
 
+  @PutMapping("/")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<Object> update(@Validated @RequestBody RegisterDTO registerData) {
+    User user = this.userService.updateUser(registerData);
+    return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(user.getId()).toUri()).build();
+  }
+
+  @GetMapping("/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<Object> find(@RequestParam(name = "id") String id) {
+    return ResponseEntity.ok(this.userService.find(id));
+  }
+
+  @DeleteMapping("/")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<Object> delete(@Validated @RequestBody LoginDTO loginDTO) {
+    this.userService.delete(loginDTO);
+    return ResponseEntity.noContent().build();
   }
 
 }
