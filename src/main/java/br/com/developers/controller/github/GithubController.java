c12a8db5c33692fcea69f43c837341ed79fb595f
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-10-03 16:32:31 -0300

    modifying api to be generic

--- src/main/java/br/com/developers/controller/github/GithubController.java
+++ src/main/java/br/com/developers/controller/github/GithubController.java
@@ -3,29 +3,32 @@ package br.com.developers.controller.github;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.developers.controller.handler.github.GithubHandler;
-import br.com.developers.github.http.resource.developer.Developer;
+import br.com.developers.github.http.service.developer.GithubService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("api/github")
+@RequestMapping(GithubController.PATH)
 public class GithubController {
 
-  private GithubHandler handler;
+  protected static final String PATH = "/api/github/";
 
-  @GetMapping("/search")
-  public ResponseEntity<Developer> search(@RequestParam(name = "qualifier") String qualifier,
+  private GithubService service;
+
+  @GetMapping("search/{endpoint}")
+  public ResponseEntity<Object> search(@PathVariable(name = "endpoint") String endpoint,
+      @RequestParam(name = "qualifier") String qualifier,
       @RequestParam(name = "sort", required = false) String sort,
       @RequestParam(name = "order", required = false) String order,
       @RequestParam(name = "per_page", required = false, defaultValue = "10") int perPage,
       @RequestParam(name = "page", required = false, defaultValue = "0") int page) {
-    return handler.searchUser(qualifier, sort, order, perPage, page);
+    return service.search(endpoint, qualifier, sort, order, perPage, page);
   }
 
 }
