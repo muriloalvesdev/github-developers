fcb71c7e4faa18fa3a3e6dc9d011371c43ba8c88
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-10-03 16:30:57 -0300

    rule to assemble the URL

--- src/main/java/br/com/developers/github/http/utils/UrlUtils.java
+++ src/main/java/br/com/developers/github/http/utils/UrlUtils.java
@@ -1,5 +1,6 @@
 package br.com.developers.github.http.utils;
 
+import java.net.URI;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -9,15 +10,38 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class UrlUtils {
 
-  public static UriComponents mountUrl(String baseUrl, String searchUsers, String qualifier,
-      String sort, String order, int perPage, int page) {
-    return UriComponentsBuilder.fromPath(baseUrl.concat(searchUsers).concat(qualifier))
-        .queryParam("sort", validateParameter(sort)).queryParam("order", validateParameter(order))
-        .queryParam("per_page", validateParameter(String.valueOf(perPage)))
-        .queryParam("page", validateParameter(String.valueOf(page))).build();
+  public static UriComponents createUrl(String endpoint, String baseUrl, String search,
+      String qualifier, String sort, String order, int perPage, int page) {
+    UriComponents uri = addParams(endpoint, baseUrl, search, qualifier, sort, order, perPage, page);
+    return uri;
   }
 
-  private static String validateParameter(String parameter) {
-    return StringUtils.isBlank(parameter) ? "" : parameter;
+  private static UriComponents addParams(String endpoint, String baseUrl, String search,
+      String qualifier, String sort, String order, int perPage, int page) {
+    UriComponentsBuilder builder = UriComponentsBuilder.newInstance();
+
+    builder.uri(URI.create(baseUrl.concat(search).concat(endpoint)));
+
+    builder.queryParam("q", qualifier);
+    if (validateParameter(sort)) {
+      builder.queryParam("sort", sort);
+    }
+
+    if (validateParameter(order)) {
+      builder.queryParam("order", order);
+    }
+
+    if (validateParameter(String.valueOf(perPage))) {
+      builder.queryParam("per_page", String.valueOf(perPage));
+    }
+
+    if (validateParameter(String.valueOf(page))) {
+      builder.queryParam("page", String.valueOf(page));
+    }
+    return builder.build(true);
+  }
+
+  private static boolean validateParameter(String parameter) {
+    return StringUtils.isNotBlank(parameter);
   }
 }
