6b8e60c5b350ccb84302e897dbf3bb37b348af0a
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-10-03 17:51:16 -0300

    activeprofiles staging in integrationTests

--- src/test/java/br/com/developers/controller/github/GithubControllerIntegrationTests.java
+++ src/test/java/br/com/developers/controller/github/GithubControllerIntegrationTests.java
@@ -9,11 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@ActiveProfiles("staging")
 class GithubControllerIntegrationTests {
 
   private static final String QUALIFIER_USER = "muriloalvesdev";
