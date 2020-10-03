c25bfa13162be0f0187ae5684f67d76bcbed6b82
muriloalvesdev <murilo.alves@idtrust.com.br>
2020-10-03 16:31:44 -0300

    adding lombok annotations to link the request information being made

--- src/main/java/br/com/developers/github/http/service/request/Send.java
+++ src/main/java/br/com/developers/github/http/service/request/Send.java
@@ -1,9 +1,9 @@
 package br.com.developers.github.http.service.request;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponents;
 
-public interface Send<T> {
-
-  ResponseEntity<T> sendRequest(UriComponents uri);
+public interface Send {
+  ResponseEntity<Object> sendRequest(UriComponents url, HttpMethod httpMethod);
 }
--- src/main/java/br/com/developers/github/http/service/request/impl/SendImpl.java
+++ src/main/java/br/com/developers/github/http/service/request/impl/SendImpl.java
@@ -1,6 +1,5 @@
 package br.com.developers.github.http.service.request.impl;
 
-import org.apache.log4j.Logger;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -8,34 +7,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
-import br.com.developers.github.http.resource.developer.Developer;
 import br.com.developers.github.http.service.request.Send;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Component
-public class SendImpl implements Send<Developer> {
-
-  private static final Logger LOG = Logger.getLogger(SendImpl.class);
-
+public class SendImpl implements Send {
   private RestTemplate restTemplate;
 
-  public ResponseEntity<Developer> sendRequest(UriComponents uri) {
-    LOG.info("Sending request to URI [ " + uri + " ]");
+  public ResponseEntity<Object> sendRequest(UriComponents url, HttpMethod httpMethod) {
+    log.info("Sending request to URI [ " + url + " ]");
     HttpHeaders headers = createHttpHeaders();
-    HttpEntity<Developer> entity = createHttpEntity(headers);
-    return this.restTemplate.exchange(uri.toUri(), HttpMethod.POST, entity, Developer.class);
+    HttpEntity<Object> entity = createHttpEntity(headers);
+    String uri = url.toUriString();
+    return this.restTemplate.exchange(uri, httpMethod, entity, Object.class);
   }
 
   private HttpHeaders createHttpHeaders() {
     HttpHeaders headers = new HttpHeaders();
-    headers.set("Accept", "application/vnd.github.v3+json");
+    headers.set("accept", "application/vnd.github.v3+json");
     return headers;
   }
 
-  private HttpEntity<Developer> createHttpEntity(HttpHeaders headers) {
-    return new HttpEntity<Developer>(headers);
+  private HttpEntity<Object> createHttpEntity(HttpHeaders headers) {
+    return new HttpEntity<Object>(headers);
   }
 
 }
